35ba148121ec23e163ef20494f62e4b30fd8b3b6
lucasem67 <lucasem80@gmail.com>
2024-12-03 14:00:03 -0300

    mudando cors

--- src/main/java/com/projetolivraria/livraria/security/cors/CorsConfig.java
+++ src/main/java/com/projetolivraria/livraria/security/cors/CorsConfig.java
@@ -10,11 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://livraria-front-end-admin.vercel.app")
-                .allowedOrigins("https://livraria-front-end-user.vercel.app/home")
+                .allowedOrigins("http://localhost:4200", "https://livraria-front-end-admin.vercel.app", "https://livraria-front-end-user.vercel.app/home")
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+                
     }
 }
